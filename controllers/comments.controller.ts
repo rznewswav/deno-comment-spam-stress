7d2e16182ebcfd87b0b204e892e502c7a27f8509
raflie <raflie@newswav.com>
2023-05-18 16:47:05 +0800

    Add spam detection mechanism

--- controllers/comments.controller.ts
+++ controllers/comments.controller.ts
@@ -1,11 +1,21 @@
 import { Controller } from "../utils/controller.ts";
 import { F, Reply, S } from "../utils/response.util.ts";
 import { DB } from "https://deno.land/x/sqlite@v3.7.2/mod.ts";
+import { RedisService } from "../redis/redis.service.ts";
+import { v5 } from "https://deno.land/std@0.109.0/uuid/mod.ts";
+
+const redis = new RedisService();
+redis
+  .start()
+  .catch(() => {
+    console.error("redis: cannot start redis");
+  });
 
 const database = new DB("./data/database.db");
 database.execute(`
 CREATE TABLE IF NOT EXISTS comments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId VARCHAR(36),
   comment TEXT,
   isHidden INTEGER DEFAULT 0,
   createdAt TIMESTAMP
@@ -16,31 +26,105 @@ export class GetCommentController implements Controller {
   method = "get";
   path = "/api/v1/comments";
   middlewares = void 0;
-  handle(_: Request): Promise<Reply> {
-    const result = database.query<[number, string, number, string]>(
-      `SELECT * FROM comments ORDER BY createdAt DESC`,
-    );
+  handle(request: Request): Promise<Reply> {
+    const url = new URL(request.url)
+    const showHidden = url.searchParams.has('showHidden')
+    let result = new Array<[number, string, string, number, string]>()
+    if (showHidden) {
+      result = database.query<typeof result[0]>(
+        `SELECT * FROM comments ORDER BY createdAt DESC`,
+      );  
+    } else {
+      result = database.query<typeof result[0]>(
+        `SELECT * FROM comments WHERE isHidden = 0 ORDER BY createdAt DESC`,
+      );  
+    }
 
     return Promise.resolve(S({
-      result: result.map(([id, comment, isHidden, createdAt]) => ({
+      result: result.map(([id, userId, comment, isHidden, createdAt]) => ({
         id,
+        userId: userId,
         comment,
         isHidden: !!isHidden,
-        createdAt: new Date(createdAt)
+        createdAt,
       })),
     }));
   }
 }
 
+const textEncoder = new TextEncoder();
+async function createV5HashFromMessage(
+  message: string,
+  namespace = "cca64aab-bc15-4064-aba4-ded7d70c4ae4",
+) {
+  return await v5.generate(namespace, textEncoder.encode(message));
+}
+
 export class CreateCommentController implements Controller {
   method = "post";
   path = "/api/v1/comments";
-  middlewares = void 0;
+
+  // two minutes
+  detectSpamWithinS = 2 * 60;
+  detectSpamWithinMs = this.detectSpamWithinS * 1000;
+  spamCountThreshold = 3;
+
+  async getUserSpamRedisKey(user: string, message: string) {
+    const hashedMessage = await createV5HashFromMessage(message);
+    return `user_spam:${user}:${hashedMessage}`;
+  }
+
+  async getSpamCount(user: string, comment: string) {
+    const key = await this.getUserSpamRedisKey(user, comment);
+    const jsonOrNull = await redis.get(key);
+    const spamStats = {
+      key,
+      count: 0,
+      comment,
+      messageIds: new Array<number>(),
+    };
+    if (jsonOrNull) {
+      try {
+        const parsedJson = JSON.parse(jsonOrNull);
+        Object.assign(spamStats, parsedJson);
+      } catch (error) {
+        console.error("error at decoding " + jsonOrNull, error);
+      }
+    }
+
+    return spamStats;
+  }
+
+  async saveSpamTracking(
+    key: string,
+    count: number,
+    comment: string,
+    messageIds: number[],
+  ) {
+    await redis.setex(
+      key,
+      this.detectSpamWithinS,
+      JSON.stringify({
+        count,
+        comment,
+        messageIds,
+      }),
+    );
+  }
+
   async handle(request: Request): Promise<Reply> {
     if (!request.headers.get("content-type")?.includes("json")) {
       return F({
         message: "requires json body instead",
       }).Status(400);
+    }
+
+    const user = request.headers.get("user");
+
+    if (!user) {
+      return F({
+        message: "unauthenticated",
+      }).Status(403);
     }
 
     const json = await request.json();
@@ -50,9 +134,35 @@ export class CreateCommentController implements Controller {
       }).Status(400);
     }
 
-    database.query(
-      `INSERT INTO comments(comment, createdAt) VALUES (?, ?)`,
-      [json.comment, new Date()],
+    const comment = json.comment;
+
+    const spamStats = await this.getSpamCount(user, comment);
+    const { key, count: spamCount } = spamStats;
+    const isSpam = spamCount >= this.spamCountThreshold;
+
+    if (isSpam) {
+      const questionMarks = Array.from({length: spamStats.messageIds.length}).map(() => "?").join(",")
+      database.query(
+        `UPDATE comments SET isHidden = 1 WHERE id in (${questionMarks})`,
+        spamStats.messageIds,
+      )
+      return F({
+        message: "spam detected",
+      }).Status(403);
+    }
+
+    const [rows] = database.query(
+      `INSERT INTO comments(userId, comment, createdAt) VALUES (?, ?, ?) RETURNING id`,
+      [user, json.comment, new Date()],
+    );
+
+    const id = rows.find(e => e) as number
+
+    await this.saveSpamTracking(
+      key,
+      spamCount + 1,
+      comment,
+      [...spamStats.messageIds, id],
     );
 
     return S({
--- main.ts
+++ main.ts
@@ -5,15 +5,8 @@ import {
 } from "./controllers/comments.controller.ts";
 const env = config();
 import { HealthController } from "./controllers/health.controller.ts";
-import { RedisService } from "./redis/redis.service.ts";
 import { Server } from "./server.ts";
 
-const redis = new RedisService();
-redis
-  .start()
-  .catch(() => {
-    console.error("redis: cannot start redis");
-  });
 
 const tryPort = Number(env.PORT);
 const port = tryPort || 3000;
